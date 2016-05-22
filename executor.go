a17f5aa204c83f0f78dfa8445234b9a5c5bf70ac
Tsuzu <tsuzu.0623@gmail.com>
2016-05-22 23:25:33 +0900

    Sun May 22 23:25:33 JST 2016

--- executor.go
+++ executor.go
@@ -162,7 +162,7 @@ func (e *Executor) Run(msTime int64, input string) ExecResult {
 				}
 			}
 
-			time.Sleep(time.Nanosecond * 10000)
+			time.Sleep(time.Nanosecond * 100 * 1000)
 		}
 	}(timerChan, execTimeChan)
 
